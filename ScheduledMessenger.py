1f9697b9e25b1db17863bb741db7b88c6bfa9171
tdeerenberg <113618658+tdeerenberg@users.noreply.github.com>
2022-09-16 01:02:15 +0200

    Add files via upload

---
+++ ScheduledMessenger.py
@@ -0,0 +1,57 @@
+# Author: tdeerenberg (C) 15 September 2022
+import pywhatkit as pwk
+import json
+import csv
+import datetime
+import os
+from time import sleep
+
+# Global variables
+now = datetime.datetime.now()
+with open("config.json", "r") as json_config:
+    config = json.load(json_config)
+
+# Logging
+def message_sent(db):
+    with open(config["SUCCESSFUL_MESSAGE_LOG"], "a") as log:
+        log.write(f"""
+Date: {now.strftime("%d %b %Y")}
+Time: {now.strftime("%X")}
+Name: {db['Name']}
+Phone: {db['Phone']}
+Message: {db['Message']}
+-------------------------
+""")
+
+def message_not_sent(db):
+    with open(config["ERROR_LOG_FILENAME"], "a") as log:
+        log.write(f"""
+Date: {now.strftime("%d %b %Y")}
+Time: {now.strftime("%X")}
+Name: {db['Name']}
+Phone: {db['Phone']}
+Message: {db['Message']}
+-------------------------
+""")
+
+# Main program
+def main():
+    with open(config["DATE_FILENAME"],'r') as data:
+        reader = csv.DictReader(data)
+        for row in reader:
+            if now.strftime('%d-%m') == row["Date"]:
+                print(f"Sending message to {row['Name']} with phone number: {row['Phone']}")
+                try:
+                    pwk.sendwhatmsg_instantly(row['Phone'], row['Message'])
+                    message_sent(row)
+                except:
+                    message_not_sent(row)
+    try: os.remove("PyWhatKit_DB.txt")
+    except: print()
+
+    ### REMOVE THE NEXT TWO LINES IF YOU USE CRONTAB, WINDOWS TASK SCHEDULER OR ANY OTHER DAILY AUTO RUN PROGRAM
+    print("The program wil sleep for 24 hours and run again. \nDO NOT CLOSE THIS PROGRAM/WINDOW. \n\nTo cancel: Ctrl+C or Kill Python")
+    sleep(86400)
+
+if __name__ == "__main__":
+    main()
